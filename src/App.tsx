633c787e33e98fdbef0be61011b7720789ba6a16
Daigo.Tokunaga <ikadamizo@gmail.com>
2018-04-15 14:59:44 +0900

    テンプレ適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import { render } from "react-dom"
+import SiteNavbar from "./components/SiteNavbar"
+import SiteContent from "./components/SiteContent"
+import SiteFooter from "./components/SiteFooter"
 
 const App = () => {
   render(
-    <div>Hello, world!</div>,
+    <React.Fragment>
+      <SiteNavbar />
+      <SiteContent />
+      <SiteFooter />
+    </React.Fragment>,
     document.getElementById("app")
   )
 }
---
+++ src/components/SiteNavbar.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+interface ISiteNavbarProps {
+  bland_logo?: string
+}
+
+const SiteNavbar :React.SFC<ISiteNavbarProps> = (props: any) => {
+  return(
+    <nav className="light-blue lighten-1" role="navigation">
+      <div className="nav-wrapper container"><a id="logo-container" href="#" className="brand-logo">Logo</a>
+        <ul className="right hide-on-med-and-down">
+          <li><a href="#">Navbar Link</a></li>
+        </ul>
+
+        <ul id="nav-mobile" className="sidenav">
+          <li><a href="#">Navbar Link</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+      </div>
+    </nav>
+  )
+}
+
+export default SiteNavbar
