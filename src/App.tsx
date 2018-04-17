aaea4d5961198f14fa2cb0b42b407c6e2d541a20
Daigo.Tokunaga <ikadamizo@gmail.com>
2018-04-17 23:42:47 +0900

    create-react-appでやり直すよ

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import * as React from "react"
-import { render } from "react-dom"
-import { Provider } from "react-redux"
-import { BrowserRouter, Route } from "react-router-dom"
-import { applyMiddleware, createStore } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
-import thunkMiddleware from "redux-thunk"
-import SiteAppBar from "./components/SiteAppBar"
-import SiteContent from "./components/SiteContent"
-import SiteFooter from "./components/SiteFooter"
+import * as React from 'react';
+import './App.css';
 
-export const store = createStore(
-  composeWithDevTools(
-    applyMiddleware(thunkMiddleware),
-  ),
-)
+import logo from './logo.svg';
 
-const Site: React.SFC = (props: any) => (
-  <React.Fragment>
-    <SiteAppBar />
-    <SiteContent />
-    <SiteFooter />
-  </React.Fragment>
-)
-
-const App = () => {
-  render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Route path="/" component={Site} />
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById("app"),
-  )
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">こんにちは React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
